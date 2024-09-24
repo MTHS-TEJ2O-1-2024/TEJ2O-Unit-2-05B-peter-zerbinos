2a1942d05c940cb7cc5daeace861a6ba3059e056
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-24 15:19:56 +0000

    created python code that shows temperature in kelvin

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,25 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Copyright (c) 2020 MTHS All rights reserved
+
+Created by: Peter Zerbinos
+Created on: Sep 2024
+This program shows the temperature in kelvin
+
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# happy face when turned on
+display.clear()
+display.show(Image.HAPPY)
+
+# pressing A will show temperature then end with happy face
+while True:
+    if button_a.is_pressed():
+        findingTemperature = round(input.temperature() + 273.1)
+        display.clear()
+        display.scroll("The temperature is:")
+        display.scroll(str(findingTemperature))
+        display.scroll("K.")
+        display.show(Image.HAPPY)
