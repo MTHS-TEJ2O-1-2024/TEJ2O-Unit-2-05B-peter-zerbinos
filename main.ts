07b349413bb0678fe286c77799f0a4dca05f0fd8
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-24 11:04:26 -0400

    created code that shows temperature in kelvin

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Sep 2024
+ * This program shows the temperature in kelvin
 */
 
-basic.showString('Hello, World!')
+//happy face when turned on
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//pressing A will show temperature then end with happy face
+input.onButtonPressed(Button.A, function () {
+    let findngTemperature = Math.round(input.temperature() + 273.15)
+    basic.clearScreen()
+    basic.showString('The temperature is:')
+    basic.showNumber(findngTemperature)
+    basic.showString('K.')
+    basic.showIcon(IconNames.Happy)
+})
